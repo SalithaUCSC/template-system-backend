9b64b8fd8ce740d3d83b52e903b7deb09a39f10f
Salitha <salithachathuranga94@gmail.com>
2022-06-10 00:08:52 +0530

    Add template models

--- src/main/java/com/ncinga/backend/models/Template.java
+++ src/main/java/com/ncinga/backend/models/Template.java
@@ -1,12 +1,16 @@
 package com.ncinga.backend.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document(collection = "templates")
+@Data
 public class Template {
-    @Id
-    Long id;
-    private String name;
-    private String description;
+    @Id String id;
+    String name;
+    String description;
+    List<Card> cards;
 }
--- src/main/java/com/ncinga/backend/repositories/TemplateRepository.java
+++ src/main/java/com/ncinga/backend/repositories/TemplateRepository.java
@@ -3,5 +3,5 @@ package com.ncinga.backend.repositories;
 import com.ncinga.backend.models.Template;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TemplateRepository extends MongoRepository<Template, Long> {
+public interface TemplateRepository extends MongoRepository<Template, String> {
 }
