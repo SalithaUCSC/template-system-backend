3742ac1b2714b3a8e4c3fb83eed878c1c52cebed
Salitha <salithachathuranga94@gmail.com>
2022-06-09 20:02:07 +0530

    Add new project

---
+++ src/main/java/com/ncinga/backend/TemplateSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ncinga.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateSystemBackendApplication.class, args);
+    }
+
+}
